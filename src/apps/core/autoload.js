6b05d170d93099a0443588da6894d41254d023ff
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2019-04-04 10:43:27 +0530

    Applied quick fixes.

--- src/apps/core/autoload.js
+++ src/apps/core/autoload.js
@@ -25,11 +25,103 @@ cmg.core.controllers.AutoloadController.prototype.autoloadActionSuccess = functi
 	if( cmt.utils.object.hasProperty( response.data, 'widgetId' ) && cmt.utils.object.hasProperty( response.data, 'widgetHtml' ) ) {
 
 		var widget = jQuery( '#' + response.data.widgetId );
+		
+		if( widget.length > 0 ) {
 
-		widget.html( response.data.widgetHtml );
+			widget.html( response.data.widgetHtml );
+		}
 	}
 };
 
 // == Direct Calls ========================
 
 // == Additional Methods ==================
+
+document.addEventListener( "DOMContentLoaded", initLazyWidgetObserver );
+
+function initLazyWidgetObserver() {
+
+	var widgets = [].slice.call( document.querySelectorAll( '.cmt-lazy-widget' ) );
+
+	// Insersection Observer support
+	if( "IntersectionObserver" in window ) {
+
+		intersectionObserver = new IntersectionObserver( function( entries, observer ) {
+
+			entries.forEach( function( entry ) {
+
+				if( entry.intersectionRatio > 0 || entry.isIntersecting ) {
+
+					target = entry.target;
+
+					processLazyWidget( target );
+
+					observer.unobserve( target );
+				}
+			});
+		});
+
+		widgets.forEach( function( lazyImage ) {
+
+			intersectionObserver.observe( lazyImage );
+		});
+	}
+	else {
+
+		// Use fallback using scroll listener
+		initLazyWidgetListener( widgets );
+	}
+}
+
+function initLazyWidgetListener( elements ) {
+
+	// Flag to relax the listener from continuous checking
+	var active = false;
+
+	// The actual listener to listen for scroll, resize and change in viewport orientation
+	var lazyLoadListener = function() {
+
+		if( active === false ) {
+
+			active = true;
+
+			setTimeout( function() {
+
+				// Iterate over the elements collection for lazy loading
+				elements.forEach( function( element ) {
+
+					// Window intersection test
+					if( ( element.getBoundingClientRect().top <= window.innerHeight && element.getBoundingClientRect().bottom >= 0 ) && getComputedStyle( element ).display !== "none" ) {
+
+						processLazyWidget( element );
+
+						// Remove from collection
+						elements = elements.filter( function( target ) {
+
+							return element !== target;
+						});
+
+						// Stop lazy loading
+						if( elements.length === 0 ) {
+
+							document.removeEventListener( 'scroll', lazyLoadListener );
+							window.removeEventListener( 'resize', lazyLoadListener );
+							window.removeEventListener( 'orientationchange', lazyLoadListener );
+						}
+					}
+				});
+
+				active = false;
+			}, 200 );
+		}
+	};
+
+	document.addEventListener( 'scroll', lazyLoadListener );
+	window.addEventListener( 'resize', lazyLoadListener );
+	window.addEventListener( 'orientationchange', lazyLoadListener );
+}
+
+function processLazyWidget( element ) {
+	
+	cmt.api.utils.request.trigger( cmt.api.root.getApplication( 'autoload' ), element, false, element.find( '.cmt-click' ) );
+}
