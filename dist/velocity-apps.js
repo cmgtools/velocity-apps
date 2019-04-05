f646668030fd5ac242f6e435918cd9030a11579a
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2019-04-05 15:28:00 +0530

    Created dist.

--- dist/velocity-apps.js
+++ dist/velocity-apps.js
@@ -1,5 +1,5 @@
 /**
- * Velocity Apps - v1.0.0-alpha1 - 2019-04-04
+ * Velocity Apps - v1.0.0-alpha1 - 2019-04-05
  * Description: Velocity Apps is application and controllers library for CMSGears.
  * License: GPL-3.0-or-later
  * Author: Bhagwat Singh Chouhan
