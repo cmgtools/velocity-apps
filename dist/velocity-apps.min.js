e8428982dfa204d1e44fddc71132d037394ee7c4
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2020-04-10 19:12:39 +0530

    Added composer.

--- dist/velocity-apps.min.js
+++ dist/velocity-apps.min.js
@@ -1,5 +1,5 @@
 /**
- * Velocity Apps - v1.0.0-alpha1 - 2020-02-27
+ * Velocity Apps - v1.0.0-alpha1 - 2020-04-10
  * Description: Velocity Apps is application and controllers library for CMSGears.
  * License: GPL-3.0-or-later
  * Author: Bhagwat Singh Chouhan
